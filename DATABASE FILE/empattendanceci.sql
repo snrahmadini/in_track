16996909c5ed2cc2fd8689091c0b70493f5b65c8
nizar1193 <149037907+nizar1193@users.noreply.github.com>
2024-06-25 11:43:55 +0700

    Add files via upload

---
+++ DATABASE FILE/empattendanceci.sql
@@ -0,0 +1,477 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Waktu pembuatan: 25 Jun 2024 pada 06.25
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `empattendanceci`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `username` char(6) NOT NULL,
+  `employee_id` int(3) UNSIGNED ZEROFILL NOT NULL,
+  `department_id` char(3) NOT NULL,
+  `shift_id` int(1) NOT NULL,
+  `location_id` int(1) NOT NULL,
+  `in_time` int(11) NOT NULL,
+  `notes` varchar(120) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `lack_of` varchar(11) NOT NULL,
+  `in_status` varchar(15) NOT NULL,
+  `out_time` int(11) NOT NULL,
+  `out_status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `username`, `employee_id`, `department_id`, `shift_id`, `location_id`, `in_time`, `notes`, `image`, `lack_of`, `in_status`, `out_time`, `out_status`) VALUES
+(45, 'ADM011', 011, 'ADM', 1, 1, 1589178316, 'sdf', 'item-200511-8f5d7be1a1.jpg', 'None', 'Late', 1589178477, 'Early'),
+(48, 'ADM011', 011, 'ADM', 1, 1, 1589381121, '', 'item-200513-ad6953a07e.jpg', 'Notes', 'Late', 1589381127, 'Over Time'),
+(49, 'PCD010', 010, 'PCD', 2, 1, 1589384432, 'asdasd', '', 'None,image', 'Late', 1589384514, 'Over Time'),
+(50, 'ADM011', 011, 'ADM', 1, 1, 1589391038, '', '', 'Notes,image', 'On Time', 1589391056, 'Early'),
+(51, 'PCD010', 010, 'PCD', 3, 1, 1622553388, 'testing', 'item-210601-3946bb00df.png', 'None', 'Late', 1622553470, 'Over Time'),
+(52, 'PCD010', 010, 'PCD', 3, 2, 1631893356, 'none', '', 'None,image', 'Late', 1631893413, 'Over Time'),
+(53, 'STD026', 026, 'STD', 1, 1, 1631894335, 'none', '', 'None,image', 'Late', 1631894403, 'Over Time'),
+(54, 'ADM011', 011, 'ADM', 1, 2, 1631894692, 'demo', '', 'None,image', 'Late', 1631894696, 'Over Time'),
+(55, 'QCD027', 027, 'QCD', 6, 2, 1631499386, 'none..', '', 'None,image', 'Late', 1631529057, 'Early'),
+(56, 'QCD027', 027, 'QCD', 6, 2, 1631583036, 'none', '', 'None,image', 'Late', 1631611849, 'Early'),
+(58, 'QCD027', 027, 'QCD', 6, 1, 1631733350, 'none', '', 'None,image', 'Late', 1631797356, 'Early'),
+(59, 'QCD027', 027, 'QCD', 6, 4, 1631863331, 'none', '', 'None,image', 'Late', 1631896539, 'Early'),
+(60, 'QCD027', 027, 'QCD', 6, 1, 1631214919, 'none', '', 'None,image', 'Late', 1631250936, 'Over Time'),
+(61, 'STD026', 026, 'STD', 1, 2, 1631493955, 'none', '', 'None,image', 'On Time', 1631523613, 'Over Time'),
+(62, 'ADM011', 011, 'ADM', 1, 1, 1631584873, 'none', '', 'None,image', 'Late', 1631621603, 'Over Time'),
+(63, 'QCD027', 027, 'QCD', 6, 2, 1632109417, 'this is a demo note!', '', 'None,image', 'Late', 1632109437, 'Early'),
+(64, 'ACD002', 002, 'ACD', 2, 3, 1632109840, 'demo demo', '', 'None,image', 'On Time', 1632109845, 'Early'),
+(65, 'STD026', 026, 'STD', 1, 2, 1632109903, 'test', '', 'None,image', 'Late', 1632109905, 'Early'),
+(66, 'HRD001', 001, 'HRD', 1, 1, 1719284686, '', '', 'Notes,image', 'Late', 1719284692, 'Early'),
+(67, 'WDT029', 029, 'WDT', 1, 1, 1719285039, 'masuk hari pertama', '', 'None,image', 'Late', 1719285053, 'Early');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `department`
+--
+
+CREATE TABLE `department` (
+  `id` char(3) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `department`
+--
+
+INSERT INTO `department` (`id`, `name`) VALUES
+('ACD', 'Accounting Department'),
+('ADM', 'Admin Department'),
+('HRD', 'Human Resource Department'),
+('PCD', 'Production Controller Department'),
+('PLD', 'Planner Department'),
+('QCD', 'Quality Control Department'),
+('SCD', 'Security Department'),
+('STD', 'Store Department'),
+('WDT', 'Warehouse Departement');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(3) UNSIGNED ZEROFILL NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `birth_date` date NOT NULL,
+  `hire_date` date NOT NULL,
+  `shift_id` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `employee`
+--
+
+INSERT INTO `employee` (`id`, `name`, `email`, `gender`, `image`, `birth_date`, `hire_date`, `shift_id`) VALUES
+(001, 'Nizar', 'devi@gmail.com', 'M', 'default.png', '2001-09-11', '2020-03-01', 1),
+(002, 'Razif', 'intan@gmail.com', 'M', 'default.png', '2001-11-01', '2020-03-01', 2),
+(003, 'Faisal', 'herman@gmail.com', 'M', 'default.png', '2001-11-21', '2020-03-12', 2),
+(004, 'Putri', 'andi@gmail.com', 'F', 'default.png', '2001-09-01', '2020-03-01', 3),
+(025, 'Admin ', 'admin@admin.com', 'M', 'default.png', '0000-00-00', '0000-00-00', 0),
+(026, 'Savira', 'christine@gmail.com', 'F', 'default.png', '2000-06-01', '2021-05-16', 1),
+(028, 'Maulana', 'iqbaldwinulhakim04@gmail.com', 'M', 'default.png', '2002-01-01', '2024-06-25', 1),
+(029, 'Rizki', 'rizki123@gmail.com', 'M', 'default.png', '2001-01-01', '2024-06-01', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `employee_department`
+--
+
+CREATE TABLE `employee_department` (
+  `id` int(3) NOT NULL,
+  `employee_id` int(3) UNSIGNED ZEROFILL NOT NULL,
+  `department_id` char(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `employee_department`
+--
+
+INSERT INTO `employee_department` (`id`, `employee_id`, `department_id`) VALUES
+(1, 001, 'HRD'),
+(2, 002, 'ACD'),
+(3, 003, 'QCD'),
+(4, 004, 'SCD'),
+(5, 005, 'STD'),
+(6, 006, 'ACD'),
+(7, 007, 'PLD'),
+(8, 008, 'STD'),
+(9, 009, 'STD'),
+(10, 010, 'PCD'),
+(21, 011, 'ADM'),
+(25, 024, 'HRD'),
+(26, 026, 'STD'),
+(27, 027, 'QCD'),
+(28, 028, 'ADM'),
+(29, 029, 'WDT');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(1) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `location`
+--
+
+INSERT INTO `location` (`id`, `name`) VALUES
+(1, 'Home'),
+(2, 'Office'),
+(3, 'Store'),
+(4, 'Site'),
+(6, 'Field'),
+(8, 'Warehouse');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `shift`
+--
+
+CREATE TABLE `shift` (
+  `id` int(1) NOT NULL,
+  `start` time NOT NULL,
+  `end` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `shift`
+--
+
+INSERT INTO `shift` (`id`, `start`, `end`) VALUES
+(1, '08:00:00', '16:00:00'),
+(2, '13:00:00', '21:00:00'),
+(3, '18:00:00', '02:00:00'),
+(5, '07:00:00', '18:25:00'),
+(6, '16:00:00', '23:59:00'),
+(7, '09:00:00', '18:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `username` char(6) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `employee_id` int(3) UNSIGNED ZEROFILL NOT NULL,
+  `role_id` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `employee_id`, `role_id`) VALUES
+('ACD002', '$2y$10$5nv5ehyMVdljfKJ6izsOqOimsbv.cbzU.XLB9ji9zbA.eICdSrNvO', 002, 2),
+('ADM011', '$2y$10$BKpQcs4XKavCcYdFWujzx.Xqb7r9eNkDrOYss2VNXrMJUUpm1agUC', 011, 2),
+('admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', 025, 1),
+('HRD001', '$2y$10$fGPRpIO8GGvTjYJbSh5Gmu5MAhtKL/vWQnOfmuQZQub.UpvQsK47O', 001, 2),
+('PCD010', '$2y$10$BKpQcs4XKavCcYdFWujzx.Xqb7r9eNkDrOYss2VNXrMJUUpm1agUC', 010, 2),
+('QCD027', '$2y$10$peALJo.JKZyD6uMBd41UfuHGQSJe7ExOfDhPITvDbSRRXeWUGY9xy', 027, 2),
+('STD005', '$2y$10$hr35h1fIySFYCSRVL2jRD.RuYa9WtJCEJkkqvQfPboYK7VwURpLim', 005, 2),
+('STD008', '$2y$10$8PGnFaiZPYtcIGrwzMmVZuNKbUb/A88f0NZOA9QVgHaUIJ6ddg.Si', 008, 2),
+('STD026', '$2y$10$8WNMvEEgNPWyRuSeeLDE1uXwnBkYNJE/heLT1zWbsUfYb/wKFyYIy', 026, 2),
+('WDT029', '$2y$10$TWPWE1xKbHI5Aqoe8cVpuOsD6gqse0DQhaEl5W1elVJl80utRn6PG', 029, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_access`
+--
+
+CREATE TABLE `user_access` (
+  `id` int(2) NOT NULL,
+  `role_id` int(1) NOT NULL,
+  `menu_id` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `user_access`
+--
+
+INSERT INTO `user_access` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 3),
+(4, 2, 4),
+(5, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(2) NOT NULL,
+  `menu` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'Master'),
+(3, 'Attendance'),
+(4, 'Profile'),
+(5, 'Report');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(1) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `name`) VALUES
+(1, 'Admin'),
+(2, 'Employee');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_submenu`
+--
+
+CREATE TABLE `user_submenu` (
+  `id` int(2) NOT NULL,
+  `menu_id` int(2) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  `icon` varchar(50) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `user_submenu`
+--
+
+INSERT INTO `user_submenu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 2, 'Department', 'master', 'fas fa-fw fa-building', 1),
+(3, 2, 'Shift', 'master/shift', 'fas fa-fw fa-exchange-alt', 1),
+(4, 2, 'Employee', 'master/employee', 'fas fa-fw fa-id-badge', 1),
+(5, 2, 'Location', 'master/location', 'fas fa-fw fa-map-marker-alt', 1),
+(6, 3, 'Attendance Form', 'attendance', 'fas fa-fw fa-clipboard-list', 1),
+(7, 3, 'Statistics', 'attendance/stats', 'fas fa-fw fa-chart-pie', 0),
+(8, 4, 'My Profile', 'profile', 'fas fa-fw fa-id-card', 1),
+(9, 2, 'Users', 'master/users', 'fas fa-fw fa-users', 1),
+(11, 5, 'Print Report', 'report', 'fas fa-fw fa-paste', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `employee_id` (`employee_id`),
+  ADD KEY `department_id` (`department_id`),
+  ADD KEY `shift_id` (`shift_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indeks untuk tabel `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `employee_department`
+--
+ALTER TABLE `employee_department`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employee_department_ibfk_1` (`employee_id`),
+  ADD KEY `employee_department_ibfk_2` (`department_id`);
+
+--
+-- Indeks untuk tabel `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `shift`
+--
+ALTER TABLE `shift`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `employee_id` (`employee_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indeks untuk tabel `user_access`
+--
+ALTER TABLE `user_access`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `menu_id` (`menu_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indeks untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user_submenu`
+--
+ALTER TABLE `user_submenu`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `menu_id` (`menu_id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+
+--
+-- AUTO_INCREMENT untuk tabel `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(3) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT untuk tabel `employee_department`
+--
+ALTER TABLE `employee_department`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT untuk tabel `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `shift`
+--
+ALTER TABLE `shift`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_access`
+--
+ALTER TABLE `user_access`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_submenu`
+--
+ALTER TABLE `user_submenu`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
