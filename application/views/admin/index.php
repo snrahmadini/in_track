9e7542e71e5f1e30862e6d8ae96d1771816276d9
Resa <resasuhendra07@gmail.com>
2025-06-15 19:18:38 +0700

    remove card

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -26,19 +26,7 @@
              </div>
            </div>
            <!-- Earnings (Monthly) Card Example -->
-           <div class="col-xl-3 col-md-6 mb-4">
-             <div class="card border-left-info shadow h-100 py-2">
-               <div class="card-body">
-                 <div class="row no-gutters align-items-center">
-                   <div class="col-auto">
-                     <i class="fas fa-exchange-alt fa-2x text-gray-300"></i>
-                   </div>
-                 </div>
-               </div>
-             </div>
-           </div>
 
-           <!-- Earnings (Monthly) Card Example -->
            <div class="col-xl-3 col-md-6 mb-4">
              <div class="card border-left-success shadow h-100 py-2">
                <div class="card-body">
