8d5f3a99f76bd0ec6ab2882abc4c1c8cd5577e14
Resa <resasuhendra07@gmail.com>
2025-06-22 22:39:43 +0700

    baseurl prod

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://localhost/in_track/";
+//$config['base_url'] = "http://localhost/in_track/";
+$config['base_url'] = "http://34.133.145.56/";
 
 /*
 |--------------------------------------------------------------------------
